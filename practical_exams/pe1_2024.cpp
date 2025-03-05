92322f6d98320986c282b278dd9dc11930bd9a70
Sh3lby007 <zhengshaobin@ymail.com>
2025-03-05 19:57:55 +0800

    format

--- practical_exams/pe1_2024.cpp
+++ practical_exams/pe1_2024.cpp
@@ -8,12 +8,17 @@ using namespace std;
 vector<string> paths(int row, int col) {
   // Your answer here
   vector<string> result;
-  if (row == 1 and col == 0) return {"N"};
-  if (row == 0 and col == 1) return {"E"};
+  if (row == 1 and col == 0)
+    return {"N"};
+  if (row == 0 and col == 1)
+    return {"E"};
   if (row > 0)
-    for (string p : paths(row - 1, col)) result.push_back("N" + p);
+    for (string p : paths(row - 1, col))
+      result.push_back("N" + p);
   if (col > 0)
-    for (string p : paths(row, col - 1)) result.push_back("E" + p);
+    for (string p : paths(row, col - 1))
+      result.push_back("E" + p);
+
   return result;
 }
 
@@ -21,13 +26,18 @@ vector<string> paths(int row, int col) {
 vector<string> paths_2(int row, int col) {
   // Your answer here
   vector<string> result;
-  if (row == 1 and col == 0) return {"N"};
-  if (row == 0 and col == 1) return {"E"};
-  if (row == 1 and col == 1) return {"NE", "EN"};
+  if (row == 1 and col == 0)
+    return {"N"};
+  if (row == 0 and col == 1)
+    return {"E"};
+  if (row == 1 and col == 1)
+    return {"NE", "EN"};
   if (row > 0) {
-    for (string p : paths_2(row - 1, col)) result.push_back("N" + p);
+    for (string p : paths_2(row - 1, col))
+      result.push_back("N" + p);
     if (col > 0)
-      for (string p : paths_2(row - 1, col - 1)) result.push_back("EN" + p);
+      for (string p : paths_2(row - 1, col - 1))
+        result.push_back("EN" + p);
   }
   return result;
 }
@@ -39,8 +49,10 @@ int points(vector<vector<int>> grid, string path) {
   int col = 0;
   int result = grid[row][col];
   for (char c : path) {
-    if (c == 'N') row -= 1;
-    if (c == 'E') col += 1;
+    if (c == 'N')
+      row -= 1;
+    if (c == 'E')
+      col += 1;
     result += grid[row][col];
   }
   return result;
@@ -52,7 +64,8 @@ int max_points(vector<vector<int>> grid) {
   int max = 0;
   for (string path : paths(grid.size() - 1, grid[0].size() - 1)) {
     int pts = points(grid, path);
-    if (pts > max) max = pts;
+    if (pts > max)
+      max = pts;
   }
   return max;
 }
