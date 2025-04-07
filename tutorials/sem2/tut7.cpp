c3dbd27c2248ede7035c1a732a256cedb771b541
Sh3lby007 <zhengshaobin@ymail.com>
2025-04-07 16:32:23 +0800

    tut 7 part 2

--- tutorials/sem2/tut7.cpp
+++ tutorials/sem2/tut7.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>  // Need to include string header
+#include <vector>
 using namespace std;
 
 class Person {
@@ -20,12 +21,65 @@ class Person {
   Person* get_mother() { return mother; }  // Return pointer
 };  // Missing semicolon after class definition
 
-int my_main() {
+class Animal {
+ private:
+  string name;
+  string sound;
+
+ public:
+  Animal(string name, string sound) {
+    this->name = name;
+    this->sound = sound;
+  }
+
+  string getName() { return name; }
+
+  string getSound() { return sound; }
+};
+
+class OldMcDonald {
+ private:
+  vector<Animal*> _farm;
+
+ public:
+  OldMcDonald() {
+    _farm.push_back(new Animal("Cow", "Moo"));
+    _farm.push_back(new Animal("Dog", "Woof"));
+    _farm.push_back(new Animal("Duck", "Quack"));
+  }
+
+  ~OldMcDonald() {
+    for (int i = 0; i < _farm.size(); i++) {
+      delete _farm[i];
+    }
+    _farm.clear();
+  }
+
+  void sing() {
+    for (int i = 0; i < _farm.size(); i++) {
+      cout << "Old McDonald had a farm, E-I-E-I-O\n";
+      cout << "And on his farm he had a " << _farm[i]->getName()
+           << ", E-I-E-I-O\n";
+      cout << "With a " << _farm[i]->getSound() << " " << _farm[i]->getSound()
+           << " here and a " << _farm[i]->getSound() << " "
+           << _farm[i]->getSound() << " there\n";
+      cout << _farm[i]->getSound() << " here " << _farm[i]->getSound()
+           << " there, everywhere " << _farm[i]->getSound() << " "
+           << _farm[i]->getSound() << "\n";
+      cout << "Old McDonald had a farm, E-I-E-I-O\n\n";
+    }
+  }
+};
+
+int main() {
   Person sally("Sally", 32);
   Person thomas("Thomas", 6, &sally);  // Need to pass address of sally
   cout << sally.get_name() << " is the mother of " << thomas.get_name()
        << endl;  // Can't access private members directly
   cout << "their age difference is " << sally.get_age() - thomas.get_age()
        << " years" << endl;
-  return 0;  // Should return a value
+
+  OldMcDonald farm;
+  farm.sing();
+  return 0;
 }
